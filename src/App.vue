f614e6d73f74c95b46efb09a0f839208d1ecc0dc
David Porosnicu <david94_p@yahoo.com>
2020-07-10 14:42:49 +0300

    add addTodo & deleteTodo

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <div class="container">
-      <Todos />
-    </div>
+    <div class="container"><AddTodo /> <Todos /></div>
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos.vue";
+import AddTodo from "./components/AddTodo.vue";
 
 export default {
   name: "app",
   components: {
     Todos,
+    AddTodo,
   },
 };
 </script>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,6 +4,7 @@
     <div class="todos">
       <div class="todo" v-for="todo in allTodos" :key="todo.id">
         {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
       </div>
     </div>
   </div>
@@ -14,7 +15,7 @@ import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Todos",
-  methods: { ...mapActions(["fetchTodos"]) },
+  methods: { ...mapActions(["fetchTodos", "deleteTodo"]) },
   computed: mapGetters(["allTodos"]),
   created() {
     this.fetchTodos();
